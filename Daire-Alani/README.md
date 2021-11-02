ed0171082e4ec8aa263725a51463662abb60b242
Orçun Berkay Akkaya <berkayakkaya49@gmail.com>
2021-11-02 18:56:03 +0300

    Delete Daire-Alani directory

--- Daire-Alani/README.md
+++
@@ -1,1 +0,0 @@
-![komut-satiri](https://user-images.githubusercontent.com/66293052/139849979-e68fe830-784a-4bae-922a-ba2ec070940d.png)
--- Daire-Alani/index.js
+++
@@ -1,9 +0,0 @@
-const arguments = process.argv.slice(2);
-
-const daireAlani = (r) => {
-    let alan;
-    alan = Math.PI * Math.pow(r, 2)
-    console.log(`Yarıçapı ${r} olan dairenin alanı: ${alan}`);
-}
-
-daireAlani(arguments[0]*1)
