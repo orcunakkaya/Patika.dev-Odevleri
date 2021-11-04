c1e29c05c9418be65e527cac092412d46fa79329
Orçun Berkay Akkaya <berkayakkaya49@gmail.com>
2021-11-04 13:43:47 +0300

    fs Module exaple

---
+++ NodeJS/Files/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+fs.appendFile('employees.json','{"name":"Employee 1 Name", "salary": 2000}' , (err)=> {
+    if (err) console.log(err);
+})
+
+fs.readFile('employees.json', 'utf8', (err, data)=> {
+    if (err) console.log(err);
+    console.log(data);
+})
+
+fs.writeFile('employees.json', '{"name":"Employee 1 Name", "salary": 4000}', 'utf8', (err)=>{
+    if (err) console.log(err);
+})
+
+fs.unlink('employees.json', (err) => {
+    if (err) console.log(err);
+})
